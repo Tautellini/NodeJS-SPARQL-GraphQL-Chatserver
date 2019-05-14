b9d3d478b978ab1549eef29ebbdeaa60015d1e42
Florian Taute <s72814@beuth-hochschule.de>
2019-05-14 14:47:17 +0200

    Fuseki Server

--- U2/server.js
+++ U2/server.js
@@ -33,6 +33,143 @@ app.use(bodyParser.json());
 
 var clients = {};
 
+function postStuff() {
+    var request = require('request');
+
+    var myjsonld = {
+        "@context" :
+        {
+            "name": "http://schema.org/name",
+            "email" : "http://schema.org/email",
+            "homepage" :
+            {
+                "@id" : "http://schema.org/url",
+                "@type" : "@id"
+            },
+            "Person" : "http://schema.org/Person/",
+        },
+        "@id" : "Person:Florian",
+        "@type" : "Person",
+        "name" : "Florian",
+        "email" : "bla@bla.de",
+        "homepage" : "http://tauteweb.de"
+    }
+
+    request.post( {
+        headers: {'content-type' : 'application/ld+json'},
+        url:'http://localhost:3030/ds/data',
+        method: 'post',
+        json: true,
+        body: myjsonld,
+        function (error, response, body) {
+            // console.log('successful update');
+            console.log(body);
+            console.log(response.statusCode)
+            console.warn(error);
+        }
+    });
+}
+
+function postStuff2() {
+    var request = require('request');
+
+    var myjsonld = {
+        "@context" :
+        {
+            "name": "http://schema.org/name",
+            "email" : "http://schema.org/email",
+            "homepage" :
+            {
+                "@id" : "http://schema.org/url",
+                "@type" : "@id"
+            },
+            "Person" : "http://schema.org/Person/",
+        },
+        "@id" : "Person:Dennis",
+        "@type" : "Person",
+        "name" : "Dennis",
+        "email" : "dennis@bla.de",
+        "homepage" : "http://dennisweb.de"
+    }
+
+    request.post( {
+        headers: {'content-type' : 'application/ld+json'},
+        url:'http://localhost:3030/ds/data',
+        method: 'post',
+        json: true,
+        body: myjsonld,
+        function (error, response, body) {
+            // console.log('successful update');
+            console.log(body);
+            console.log(response.statusCode)
+            console.warn(error);
+        }
+    });
+}
+
+function postMessage() {
+    var request = require('request');
+
+    var sender = "Florian";
+    var recipient = "Dennis";
+
+    var myjsonld = {
+        "@context" :
+        {
+            "Messages" : "http://schema.org/Messages/",
+            "Message" : {
+                "@id" : "http://schema.org/Message",
+                "@type" : "@id",
+                "sender" : "http://schema.org/Person/",
+                "recipient" : "http://schema.org/Person/",
+            }
+        },
+        "@id" : "Messages:"+sender+recipient,
+        "@type" : "Messages",
+        "Message" : {
+            "text" : "Lorem Ipsum Dolor Sit Achim",
+            "sender" : "http://schema.org/Person/"+sender,
+            "recipient" : "http://schema.org/Person/"+recipient,
+        }
+    }
+
+    request.post( {
+        headers: {'content-type' : 'application/ld+json'},
+        url:'http://localhost:3030/ds/data',
+        method: 'post',
+        json: true,
+        body: myjsonld,
+        function (error, response, body) {
+            // console.log('successful update');
+            console.log(body);
+            console.log(response.statusCode)
+            console.warn(error);
+        }
+    });
+}
+
+
+function postSparqlQuery(query) {
+    var http = require('http');
+
+    var options = {
+        host: 'localhost',
+        path: '/ds/query?update=',
+        port: '3030',
+        method: 'POST'
+    };
+
+    var req = http.request(options, function(res) {
+        res.setEncoding("utf8");
+        res.on('data', function(chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+    //This is the data we are posting, it needs to be a string or a buffer
+    req.write("PREFIX test:<http://www.semanticweb.org/muhammad/ontologies/2017/2/untitled-ontology-14#> INSERT { ?KPIs test:hasValue 2009} WHERE { ?KPIs test:hasValue ?Newvalue}");
+    req.end();
+}
+
 app.ws('/', (client, req) => {
     client.on('message', message => {
         var parsedJson = JSON.parse(message);
@@ -40,6 +177,10 @@ app.ws('/', (client, req) => {
         console.log(parsedJson);
         if ("name" in parsedJson) {
             clients[parsedJson.name] = client;
+            // postSparqlQuery(parsedJson);
+            postStuff();
+            postStuff2();
+            postMessage();
         } else {
             var key = parsedJson.recipient;
             if (!(clients[key] === undefined)) {
@@ -50,7 +191,7 @@ app.ws('/', (client, req) => {
         }
     });
     client.on('close', () => {
-        
+
         console.log("Connection Closed");
     });
 });
