6ba4729b00ac32e3ad3cf169a4a02f397184ab90
Florian Taute <s72814@beuth-hochschule.de>
2019-06-09 03:33:56 +0200

    Routen hinzugefügt

--- U3/app.js
+++ U3/app.js
@@ -7,7 +7,7 @@
 /*****************************************************************/
 
 /*
-TEST DATA ON SERVER IN THE FOLLOWING FORM:
+ADD TEST DATA ON SERVER IN THE FOLLOWING FORM, SERVER NOT PERSISTENT WHILE PRODUCTION:
 PREFIX schema: <http://schema.org/>
 INSERT DATA
 { <http://localhost:3030/users> schema:user <http://localhost:3030/users/101> .
@@ -83,6 +83,12 @@ function sparqlQuery (method, action, query, data) {
         WHERE {
         <http://localhost:3030/users/${data.userid}/tweets/${data.tweetid}> schema:text ?text
         }`
+        var queryPostTweet = `
+        PREFIX schema: <http://schema.org/>
+        SELECT (<http://localhost:3030/users/${data.userid}/tweets/${data.tweetid}>) ?text
+        WHERE {
+        <http://localhost:3030/users/${data.userid}/tweets/${data.tweetid}> schema:text ?text
+        }`
         /***************************************/
         /*           QUERY SETTERS             */
         /***************************************/
@@ -97,14 +103,20 @@ function sparqlQuery (method, action, query, data) {
         if (action == "postuser") {
             query = queryPostUser;
         }
+        if (action == "deleteuser") {
+            // Todo
+        }
         if (action == "gettweets") {
             query = queryGetTweets;
         }
         if (action == "gettweet") {
             query = queryGetTweet;
         }
-        if (action == "puttweet") {
-            query = queryPutTweet;
+        if (action == "posttweet") {
+            query = queryPostTweet;
+        }
+        if (action == "deletetweet") {
+            // Todo
         }
         /***************************************/
         /*           SPARQL METHODS            */
@@ -131,6 +143,9 @@ function sparqlQuery (method, action, query, data) {
         })
 }
 
+/***************************************/
+/*           SPARQL BLOCK              */
+/***************************************/
 app.route('/sparql')
     .get( async (req, res) => {
         if (req.query != null) {
@@ -143,59 +158,75 @@ app.route('/sparql')
     .post((req, res) => {
     })
 
+/***************************************/
+/*          GRAPHQL METHODS            */
+/***************************************/
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-/* app.get('/graphql', (req,res, next) => {
-    var request = require('request');
-    // var Converter = require('graphql-to-sparql').Converter;
-    // var algebra = new Converter().graphqlToSparqlAlgebra('{user{name}}', {
-    //     "user" : "http://schema.org/user",
-    //     "name": "http://schema.org/name"
-    // })
-
-    console.log(req.query.query);
-}) */
-
-    const graphqlHTTP = require('express-graphql');
-    const { buildSchema } = require('graphql');
-
-     const schema = buildSchema(`
-        type Query {
-           users: String
-           user (id: Int!): String
-           tweets: String
-           tweet: String
-       }
-    `);
-
-    const root = {
-        users: async () => {
-            let data = await sparqlQuery("get", "getusers", "")
-            return JSON.stringify(data)
-        },
-        user: async (id) => {
-            let data = await sparqlQuery("get", "getuser", "", id)
-            return JSON.stringify(data)
-        },
-        tweets: async () => {
-            let data = await sparqlQuery("get", "gettweets", "")
-            return JSON.stringify(data)
-        },
-        tweet: async (id) => {
-            let data = await sparqlQuery("get", "gettweet", "", id)
-            return JSON.stringify(data)
-        }
+const schema = buildSchema(`
+    input UserInput {
+        name: String!
     }
+    input TweetInput {
+        userid: Int!
+        text: String!
+    }
+    type Query {
+        users: String
+        user (id: Int!): String
+        tweets: String
+        tweet (id: Int!): String
+    }
+    type Mutation {
+        createUser(input: UserInput): String
+        createTweet(input: TweetInput): String
+    }
+    `)
+    // Todo: deleteUser and deleteTweet
+const root = {
+    users: async () => {
+        let data = await sparqlQuery("get", "getusers", "")
+        return JSON.stringify(data)
+    },
+    user: async (id) => {
+        let data = await sparqlQuery("get", "getuser", "", id)
+        return JSON.stringify(data)
+    },
+    tweets: async () => {
+        let data = await sparqlQuery("get", "gettweets", "")
+        return JSON.stringify(data)
+    },
+    tweet: async (id) => {
+        let data = await sparqlQuery("get", "gettweet", "", id)
+        return JSON.stringify(data)
+    },
+    createUser: async (user) => {
+        let tempUsers = await sparqlQuery("get", "getusers", "","")
+        user.id = tempUsers.length+101
+        let data = await sparqlQuery("post", "postuser", "", user)
+        return JSON.stringify(data)
+    },
+    createTweet: (tweet) => {
+        // Todo
+    },
+    deleteUser: (user) => {
+        // Todo
+    },
+    deleteTweet: (tweet) => {
+        // Todo
+    }
+}
 
-    app.use('/graphql', graphqlHTTP({
-       schema,
-       rootValue: root,
-       graphiql: true,
-    }))
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+}))
 
 /***************************************/
-/*             users block             */
+/*             USERS BLOCK             */
 /***************************************/
-/* route = /users */
 app.route('/users')
     .get( async (req, res) => {
         let data = await sparqlQuery("get", "getusers", "", "")
@@ -207,7 +238,7 @@ app.route('/users')
     })
     .post( async (req, res) => {
         let tempUsers = await sparqlQuery("get", "getusers", "","")
-        req.body.id = tempUsers.length
+        req.body.id = tempUsers.length+101
         let data = await sparqlQuery("post", "postuser", "", req.body)
         res.send(data)
     })
@@ -216,43 +247,25 @@ app.route('/users')
         res.send('Operation not allowed')
     })
 
-/* route = /users/:id */
 app.route('/users/:id')
     .get( async (req, res) => {
         let data = await sparqlQuery("get", "getuser", null, req.params)
         res.send(data);
     })
     .put((req, res) => {
-        console.log("app.put auf users:" + req.params.id + " wurde aufgerufen. Das neue Objekt sieht folgendermaßen aus: " + JSON.stringify(req.body))
-        let newUser = req.body
-        console.log("Die ID des neuen Objekts wird ausgelesen und die Felder href und tweets werden entsprechend richtig gesetzt.")
-        newUser.id = Number(req.params.id)
-        newUser.href = ("http://localhost/users/" + newUser.id)
-        newUser.tweets = ("http://localhost/users/" + newUser.id + "/tweets")
-        console.log("Ersetzung durch neues Objekt wird im Store durchgeführt.")
-        store.replace("users", newUser.id, newUser)
-        console.log(JSON.stringify(newUser))
-        res.contentType('application/json').send(JSON.stringify(newUser))
+        // Todo
     })
     .post((req, res) => {
         console.log("app.post auf users:" + req.params.id + " wurde aufgerufen. Post auf spezielle ID ist nicht gestattet. Fehlermeldung wird ausgeliefert.")
         res.send('Operation not allowed')
     })
     .delete((req, res) => {
-        console.log("app.delete auf users:" + req.params.id + " wurde aufgerufen. Entferne das Objekt mit der gegebenen ID aus dem Store.")
-        store.remove("users", req.params.id)
-        if (store.select("users", req.params.id))
-            res.send('Objekt konnte nicht entfernt werden.')
-        else
-            res.send('Objekt erfolgreich entfernt.')
+        // Todo
     })
 
-
 /***************************************/
-/*              tweets  block          */
+/*         tweets  block               */
 /***************************************/
-
-/* route = /tweets */
 app.route('/tweets')
     .get((req, res) => {
         console.log("app.get auf /tweets wurde aufgerufen. Liefere alle tweets aus.")
@@ -264,33 +277,24 @@ app.route('/tweets')
         res.send('Operation not allowed')
     })
     .post((req, res) =>{
-        console.log("app.post auf /tweets wurde aufgerufen. Das folgende Objekt wurde erhalten: \n " + JSON.stringify(req.body))
-        let newtweet = req.body
-        let user = newtweet.user
-        if (user) {
-            userID = newtweet.user.substring(newtweet.user.lastIndexOf('/') + 1)
-            console.log("Das Feld user wurde gesetzt. Überprüfe ob der User " + newtweet.user + " bekannt ist.")
-            selectedUser =  store.select("users", userID)
-            if (selectedUser) {
-                console.log("Der User ist bekannt. Der tweet wird gespeichert.")
-                newtweet =  store.insert("tweets", newtweet)
-                newtweet.href = ("http://localhost/tweets/" + newtweet.id)
-                newtweet =  store.replace("tweets", newtweet.id, newtweet)
-                res.contentType('application/json').send(JSON.stringify(newtweet))
-            }
-            else
-                res.contentType('text/plain').send("Unknown user id " + userID)
-        }
+        // Todo
     })
     .delete((req, res) => {
         console.log("app.delete auf /tweets wurde aufgerunfen. Das Löschen aller Tweets ist nicht erlaubt.")
         res.send('Operation not allowed')
     })
 
-/* route = /tweets/:id */
 app.all('/tweets/:id', (req,res) => {
     res.send("Operation not allowed. Use /users/:userid/tweets/:tweetid instead")
 })
+
+/***************************************/
+/*      tweets of user block           */
+/***************************************/
+app.route('/users/:id/tweets')
+    .get((req, res) => {
+        // Todo
+    })
 
 app.route('/users/:userid/tweets/:tweetid')
     .get( async (req, res) => {
@@ -298,47 +302,15 @@ app.route('/users/:userid/tweets/:tweetid')
         res.send(data);
     })
     .put((req, res) => {
-        console.log("app.put auf tweets:" + req.params.id + " wurde aufgerufen. Überschreiben mit folgendem Objekt:\n" + JSON.stringify(req.body))
-        let newtweet = req.body
-        newtweet.id = Number(req.params.id)
-        newtweet.href = ("http://localhost/tweets/" + newtweet.id)
-        store.replace("tweets", newtweet.id, newtweet)
-        res.contentType('application/json').send(JSON.stringify(newtweet))
+        // Todo
     })
     .post((req, res) => {
         console.log("app.put auf tweets:" + req.params.id + " wurde aufgerufen. POST auf ID ist nicht erlaubt.")
         res.send('Operation not allowed')
     })
     .delete((req, res) =>  {
-        console.log("app.delete auf tweets:" + req.params.id + " wurde aufgerufen. Das tweet mit der entsprechenden ID wird aus dem Store gelöscht.")
-        store.remove("tweets", req.params.id)
-        if (store.select("tweets", req.params.id))
-            res.send('Objekt konnte nicht entfernt werden.')
-        else
-            res.send('Objekt wurde erfolgreich gelöscht.')
+        // Todo
     })
-
-
-
-/***************************************/
-/*            tweets of user block     */
-/***************************************/
-app.route('/users/:id/tweets')
-    .get((req, res) => {
-        console.log("app.get auf users:" + req.params.id + " wurde aufgerunfen. Überprüfe ob der User bekannt ist.")
-        let selectedUser = store.select("users", req.params.id)
-        if (selectedUser) {
-            let tweets = store.select('tweets')
-            console.log("Der User ist bekannt. Alle tweets auf gefiltert auf user: " + selectedUser.href)
-            tweets = tweets.filter(item => item.user == selectedUser.href);
-            console.log("Folgende tweets wurden gefunden und werden ausgeliefert:\n" + JSON.stringify(tweets))
-            res.contentType('application/json').send(tweets)
-        }
-        else
-            res.send("user not known with id " + req.params.id)
-    })
-
-
 
 /******************************************/
 /*       express.static, start server     */
